0e9f98a25e200a9b5fbcfc73793605eb575368ee
Evan Danaher <edanaher@ooyala.com>
2014-05-16 11:22:02 -0700

    Support local builds with file:// repos.

--- src/atlantis/builder/git/client.go
+++ src/atlantis/builder/git/client.go
@@ -13,11 +13,19 @@ type Info struct {
 	RevList []string `json:"rev_list"`
 }
 
-func Checkout(url, sha, path string) Info {
-	if err := os.Chdir(path); err != nil {
-		panic(err)
-	}
+func checkShaExists(sha string) bool {
+	cmd := exec.Command("git", "rev-list", "--all")
+	out := util.EchoExec(cmd)
 
+	for _, s := range strings.Split(string(out), "\n") {
+		if strings.Trim(s, "\n") == sha {
+			return true
+		}
+	}
+	return false
+}
+
+func fancyCheckout(url string, sha string) {
 	cmd := exec.Command("git", "init")
 	util.EchoExec(cmd)
 
@@ -27,17 +35,7 @@ func Checkout(url, sha, path string) Info {
 	cmd = exec.Command("git", "remote", "update")
 	util.EchoExec(cmd)
 
-	cmd = exec.Command("git", "rev-list", "--all")
-	out := util.EchoExec(cmd)
-
-	var found bool
-	for _, s := range strings.Split(string(out), "\n") {
-		if strings.Trim(s, "\n") == sha {
-			found = true
-			break
-		}
-	}
-	if !found {
+	if !checkShaExists(sha) {
 		panic("sha " + sha + " not found in repository!")
 	}
 
@@ -47,16 +45,44 @@ func Checkout(url, sha, path string) Info {
 	cmd = exec.Command("git", "reset", "--hard", sha)
 	util.EchoExec(cmd)
 
-	cmd = exec.Command("git", "show-branch", "--list")
-	out = util.EchoExec(cmd)
+	cmd = exec.Command("git", "submodule", "update", "--init")
+	util.EchoExec(cmd)
+}
+
+func localCheckout(path string, sha string) {
+	// Rsync with a trailing slash won't create a subdirectory
+	cmd := exec.Command("rsync", "-a", path+"/", ".")
+	util.EchoExec(cmd)
+
+	if !checkShaExists(sha) {
+		panic("sha " + sha + " not found in repository!")
+	}
+
+	cmd = exec.Command("git", "reset", "--hard", sha)
+	util.EchoExec(cmd)
+}
+
+func Checkout(url, sha, path string) Info {
+	if err := os.Chdir(path); err != nil {
+		panic(err)
+	}
+
+	scheme := strings.SplitN(url, ":", 2)[0]
+
+	if scheme == "file" {
+		path := strings.TrimPrefix(url, "file://")
+		localCheckout(path, sha)
+	} else {
+		fancyCheckout(url, sha)
+	}
+
+	cmd := exec.Command("git", "show-branch", "--list")
+	out := util.EchoExec(cmd)
 	commit := strings.Split(string(out), "\n")[0]
 
 	cmd = exec.Command("git", "log", "--pretty=format:%H")
 	out = util.EchoExec(cmd)
 	revlist := strings.Split(string(out), "\n")
-
-	cmd = exec.Command("git", "submodule", "update", "--init")
-	util.EchoExec(cmd)
 
 	return Info{
 		Commit:  commit,
